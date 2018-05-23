a59f479ae742de5abc92ddec978108bc79586cbf
Guillaume Chainet <gchainet@gmail.com>
2018-05-23 10:47:58 +0200

    Remove useless this keyword

--- sources/vec3f.cpp
+++ sources/vec3f.cpp
@@ -16,15 +16,15 @@ Vec3f::Vec3f(float x, float y, float z) :
 Vec3f::~Vec3f()
 {}
 
-float Vec3f::x() const { return this->m_x; }
+float Vec3f::x() const { return m_x; }
 
-float Vec3f::y() const { return this->m_y; }
+float Vec3f::y() const { return m_y; }
 
-float Vec3f::z() const { return this->m_z; }
+float Vec3f::z() const { return m_z; }
 
-void Vec3f::setX(float f) { this->m_x = f; }
-void Vec3f::setY(float f) { this->m_y = f; }
-void Vec3f::setZ(float f) { this->m_z = f; }
+void Vec3f::setX(float f) { m_x = f; }
+void Vec3f::setY(float f) { m_y = f; }
+void Vec3f::setZ(float f) { m_z = f; }
 
 Vec3f &Vec3f::normalize()
 {
@@ -41,65 +41,65 @@ Vec3f &Vec3f::normalize()
 
 float Vec3f::norm() const
 {
-  return sqrt(this->m_x * this->m_x + this->m_y * this->m_y + this->m_z * this->m_z);
+  return sqrt(m_x * m_x + m_y * m_y + m_z * m_z);
 }
 
 float Vec3f::norm2() const
 {
-  return this->m_x * this->m_x + this->m_y * this->m_y + this->m_z * this->m_z;
+  return m_x * m_x + m_y * m_y + m_z * m_z;
 }
 
 Vec3f Vec3f::operator*(const float &f) const
 {
-  return Vec3f(this->m_x * f, this->m_y * f, this->m_z * f);
+  return Vec3f(m_x * f, m_y * f, m_z * f);
 }
 
 float Vec3f::operator*(const Vec3f &v) const
 {
-  return this->m_x * v.x() + this->m_y * v.y() + this->m_z * v.z();
+  return m_x * v.x() + m_y * v.y() + m_z * v.z();
 }
 
 Vec3f Vec3f::operator+(const Vec3f &v) const
 {
-  return Vec3f(this->m_x + v.x(), this->m_y + v.y(), this->m_z + v.z());
+  return Vec3f(m_x + v.x(), m_y + v.y(), m_z + v.z());
 }
 
 Vec3f Vec3f::operator-(const Vec3f &v) const
 {
-  return Vec3f(this->m_x - v.x(), this->m_y - v.y(), this->m_z - v.z());
+  return Vec3f(m_x - v.x(), m_y - v.y(), m_z - v.z());
 }
 
 Vec3f Vec3f::operator-(const float &f) const
 {
-  return Vec3f(this->m_x - f, this->m_y - f, this->m_z - f);
+  return Vec3f(m_x - f, m_y - f, m_z - f);
 }
 
 
 Vec3f Vec3f::operator+=(const Vec3f &v)
 {
-  this->m_x += v.x();
-  this->m_y += v.y();
-  this->m_z += v.z();
+  m_x += v.x();
+  m_y += v.y();
+  m_z += v.z();
   return *this;
 }
 
 Vec3f Vec3f::operator-=(const Vec3f &v)
 {
-  this->m_x -= v.x();
-  this->m_y -= v.y();
-  this->m_z -= v.z();
+  m_x -= v.x();
+  m_y -= v.y();
+  m_z -= v.z();
   return *this;
 }
 
 Vec3f Vec3f::operator*=(const float &f)
 {
-  this->m_x *= f;
-  this->m_y *= f;
-  this->m_z *= f;
+  m_x *= f;
+  m_y *= f;
+  m_z *= f;
   return *this;
 }
 
 Vec3f Vec3f::operator-() const
 {
-  return Vec3f(- this->m_x, - this->m_y, - this->m_z);
+  return Vec3f(- m_x, - m_y, - m_z);
 }
